aba307ecc4e5a4efb359a1a744f364bc45b480d2
Apple <1124470898@qq.com>
2017-04-10 14:57:53 +0800

    初版

---
+++ musicUtopiaIOS/musicUtopiaIOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  musicUtopiaIOS
+//
+//  Created by Apple on 2017/4/10.
+//  Copyright © 2017年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ musicUtopiaIOS/musicUtopiaIOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  musicUtopiaIOS
+//
+//  Created by Apple on 2017/4/10.
+//  Copyright © 2017年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
